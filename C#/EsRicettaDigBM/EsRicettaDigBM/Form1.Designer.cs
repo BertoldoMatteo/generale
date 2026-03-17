ef350b2bae9fd139c0b7b95308e9e672fa7cfde9
BertoldoMatteo <14529942@itisrossi.vi.it>
2026-03-17 20:52:44 +0100

    prepara esRicett

---
+++ C#/EsRicettaDigBM/EsRicettaDigBM/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace EsRicettaDigBM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitolo = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblEtichetta = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitolo
+            // 
+            this.lblTitolo.AutoSize = true;
+            this.lblTitolo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitolo.Location = new System.Drawing.Point(322, 3);
+            this.lblTitolo.Name = "lblTitolo";
+            this.lblTitolo.Size = new System.Drawing.Size(135, 24);
+            this.lblTitolo.TabIndex = 1;
+            this.lblTitolo.Text = "1001 Crostata";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(146, 30);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(505, 308);
+            this.listBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Yellow;
+            this.panel1.Location = new System.Drawing.Point(30, 30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(100, 300);
+            this.panel1.TabIndex = 3;
+            // 
+            // lblEtichetta
+            // 
+            this.lblEtichetta.AutoSize = true;
+            this.lblEtichetta.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEtichetta.Location = new System.Drawing.Point(612, 417);
+            this.lblEtichetta.Name = "lblEtichetta";
+            this.lblEtichetta.Size = new System.Drawing.Size(176, 24);
+            this.lblEtichetta.TabIndex = 4;
+            this.lblEtichetta.Text = "BERTOLDO MATTEO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEtichetta);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lblTitolo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitolo;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblEtichetta;
+    }
+}
+
