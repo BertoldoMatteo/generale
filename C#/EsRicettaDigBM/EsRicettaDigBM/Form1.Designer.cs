f8999fece5df75a3b1dcb940b25d1cf94f4d3c3a
BertoldoMatteo <14529942@itisrossi.vi.it>
2026-03-18 20:56:20 +0100

    ultimo

--- C#/EsRicettaDigBM/EsRicettaDigBM/Form1.Designer.cs
+++ C#/EsRicettaDigBM/EsRicettaDigBM/Form1.Designer.cs
@@ -29,9 +29,26 @@
         private void InitializeComponent()
         {
             this.lblTitolo = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.lstElenco = new System.Windows.Forms.ListBox();
             this.lblEtichetta = new System.Windows.Forms.Label();
+            this.lblFrolla = new System.Windows.Forms.Label();
+            this.lblMarmellata = new System.Windows.Forms.Label();
+            this.lblCottura = new System.Windows.Forms.Label();
+            this.lblDiametro = new System.Windows.Forms.Label();
+            this.lblDecorazione = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.cmbFrolla = new System.Windows.Forms.ComboBox();
+            this.cmbMarmellata = new System.Windows.Forms.ComboBox();
+            this.txtTempoCottura = new System.Windows.Forms.TextBox();
+            this.txtDiametro = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.rdbT = new System.Windows.Forms.RadioButton();
+            this.rdbF = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRicerca = new System.Windows.Forms.Button();
+            this.btnAggiungi = new System.Windows.Forms.Button();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitolo
@@ -44,45 +61,208 @@
             this.lblTitolo.TabIndex = 1;
             this.lblTitolo.Text = "1001 Crostata";
             // 
-            // listBox1
+            // lstElenco
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(146, 30);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(505, 308);
-            this.listBox1.TabIndex = 2;
-            // 
-            // panel1
-            // 
-            this.panel1.BackColor = System.Drawing.Color.Yellow;
-            this.panel1.Location = new System.Drawing.Point(30, 30);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(100, 300);
-            this.panel1.TabIndex = 3;
+            this.lstElenco.FormattingEnabled = true;
+            this.lstElenco.ItemHeight = 16;
+            this.lstElenco.Location = new System.Drawing.Point(12, 30);
+            this.lstElenco.Name = "lstElenco";
+            this.lstElenco.Size = new System.Drawing.Size(600, 340);
+            this.lstElenco.TabIndex = 2;
             // 
             // lblEtichetta
             // 
             this.lblEtichetta.AutoSize = true;
             this.lblEtichetta.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEtichetta.Location = new System.Drawing.Point(612, 417);
+            this.lblEtichetta.Location = new System.Drawing.Point(778, 458);
             this.lblEtichetta.Name = "lblEtichetta";
             this.lblEtichetta.Size = new System.Drawing.Size(176, 24);
             this.lblEtichetta.TabIndex = 4;
             this.lblEtichetta.Text = "BERTOLDO MATTEO";
+            // 
+            // lblFrolla
+            // 
+            this.lblFrolla.AutoSize = true;
+            this.lblFrolla.Location = new System.Drawing.Point(14, 10);
+            this.lblFrolla.Name = "lblFrolla";
+            this.lblFrolla.Size = new System.Drawing.Size(41, 16);
+            this.lblFrolla.TabIndex = 5;
+            this.lblFrolla.Text = "Frolla";
+            // 
+            // lblMarmellata
+            // 
+            this.lblMarmellata.AutoSize = true;
+            this.lblMarmellata.Location = new System.Drawing.Point(177, 10);
+            this.lblMarmellata.Name = "lblMarmellata";
+            this.lblMarmellata.Size = new System.Drawing.Size(74, 16);
+            this.lblMarmellata.TabIndex = 6;
+            this.lblMarmellata.Text = "Marmellata";
+            // 
+            // lblCottura
+            // 
+            this.lblCottura.AutoSize = true;
+            this.lblCottura.Location = new System.Drawing.Point(14, 75);
+            this.lblCottura.Name = "lblCottura";
+            this.lblCottura.Size = new System.Drawing.Size(140, 16);
+            this.lblCottura.TabIndex = 7;
+            this.lblCottura.Text = "Tempo di cottura (min)";
+            // 
+            // lblDiametro
+            // 
+            this.lblDiametro.AutoSize = true;
+            this.lblDiametro.Location = new System.Drawing.Point(177, 75);
+            this.lblDiametro.Name = "lblDiametro";
+            this.lblDiametro.Size = new System.Drawing.Size(91, 16);
+            this.lblDiametro.TabIndex = 8;
+            this.lblDiametro.Text = "Diametro (cm)";
+            // 
+            // lblDecorazione
+            // 
+            this.lblDecorazione.AutoSize = true;
+            this.lblDecorazione.Location = new System.Drawing.Point(14, 135);
+            this.lblDecorazione.Name = "lblDecorazione";
+            this.lblDecorazione.Size = new System.Drawing.Size(84, 16);
+            this.lblDecorazione.TabIndex = 9;
+            this.lblDecorazione.Text = "Decorazione";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(177, 135);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(44, 16);
+            this.lblNome.TabIndex = 10;
+            this.lblNome.Text = "Nome";
+            // 
+            // cmbFrolla
+            // 
+            this.cmbFrolla.FormattingEnabled = true;
+            this.cmbFrolla.Location = new System.Drawing.Point(18, 37);
+            this.cmbFrolla.Name = "cmbFrolla";
+            this.cmbFrolla.Size = new System.Drawing.Size(121, 24);
+            this.cmbFrolla.TabIndex = 11;
+            // 
+            // cmbMarmellata
+            // 
+            this.cmbMarmellata.FormattingEnabled = true;
+            this.cmbMarmellata.Location = new System.Drawing.Point(180, 37);
+            this.cmbMarmellata.Name = "cmbMarmellata";
+            this.cmbMarmellata.Size = new System.Drawing.Size(121, 24);
+            this.cmbMarmellata.TabIndex = 12;
+            // 
+            // txtTempoCottura
+            // 
+            this.txtTempoCottura.Location = new System.Drawing.Point(24, 106);
+            this.txtTempoCottura.Name = "txtTempoCottura";
+            this.txtTempoCottura.Size = new System.Drawing.Size(100, 22);
+            this.txtTempoCottura.TabIndex = 13;
+            this.txtTempoCottura.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTempoCottura_KeyPress);
+            // 
+            // txtDiametro
+            // 
+            this.txtDiametro.Location = new System.Drawing.Point(180, 106);
+            this.txtDiametro.Name = "txtDiametro";
+            this.txtDiametro.Size = new System.Drawing.Size(100, 22);
+            this.txtDiametro.TabIndex = 14;
+            this.txtDiametro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDiametro_KeyPress);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(180, 154);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 22);
+            this.txtNome.TabIndex = 15;
+            // 
+            // rdbT
+            // 
+            this.rdbT.AutoSize = true;
+            this.rdbT.Location = new System.Drawing.Point(18, 156);
+            this.rdbT.Name = "rdbT";
+            this.rdbT.Size = new System.Drawing.Size(40, 20);
+            this.rdbT.TabIndex = 16;
+            this.rdbT.TabStop = true;
+            this.rdbT.Text = "SI";
+            this.rdbT.UseVisualStyleBackColor = true;
+            // 
+            // rdbF
+            // 
+            this.rdbF.AutoSize = true;
+            this.rdbF.Location = new System.Drawing.Point(18, 182);
+            this.rdbF.Name = "rdbF";
+            this.rdbF.Size = new System.Drawing.Size(48, 20);
+            this.rdbF.TabIndex = 17;
+            this.rdbF.TabStop = true;
+            this.rdbF.Text = "NO";
+            this.rdbF.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel2.Controls.Add(this.lblFrolla);
+            this.panel2.Controls.Add(this.rdbF);
+            this.panel2.Controls.Add(this.lblMarmellata);
+            this.panel2.Controls.Add(this.rdbT);
+            this.panel2.Controls.Add(this.lblCottura);
+            this.panel2.Controls.Add(this.txtNome);
+            this.panel2.Controls.Add(this.lblDiametro);
+            this.panel2.Controls.Add(this.txtDiametro);
+            this.panel2.Controls.Add(this.lblDecorazione);
+            this.panel2.Controls.Add(this.txtTempoCottura);
+            this.panel2.Controls.Add(this.lblNome);
+            this.panel2.Controls.Add(this.cmbMarmellata);
+            this.panel2.Controls.Add(this.cmbFrolla);
+            this.panel2.Location = new System.Drawing.Point(618, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(315, 241);
+            this.panel2.TabIndex = 18;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(312, 390);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 50);
+            this.btnSave.TabIndex = 18;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSsave_Click);
+            // 
+            // btnRicerca
+            // 
+            this.btnRicerca.Location = new System.Drawing.Point(162, 390);
+            this.btnRicerca.Name = "btnRicerca";
+            this.btnRicerca.Size = new System.Drawing.Size(100, 50);
+            this.btnRicerca.TabIndex = 19;
+            this.btnRicerca.Text = "RICERCA";
+            this.btnRicerca.UseVisualStyleBackColor = true;
+            this.btnRicerca.Click += new System.EventHandler(this.btnRicerca_Click);
+            // 
+            // btnAggiungi
+            // 
+            this.btnAggiungi.Location = new System.Drawing.Point(713, 288);
+            this.btnAggiungi.Name = "btnAggiungi";
+            this.btnAggiungi.Size = new System.Drawing.Size(100, 50);
+            this.btnAggiungi.TabIndex = 21;
+            this.btnAggiungi.Text = "AGGIUNGI";
+            this.btnAggiungi.UseVisualStyleBackColor = true;
+            this.btnAggiungi.Click += new System.EventHandler(this.btnAggiungi_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Goldenrod;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(966, 491);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAggiungi);
+            this.Controls.Add(this.btnRicerca);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.lblEtichetta);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstElenco);
             this.Controls.Add(this.lblTitolo);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,9 +271,25 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitolo;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox lstElenco;
         private System.Windows.Forms.Label lblEtichetta;
+        private System.Windows.Forms.Label lblFrolla;
+        private System.Windows.Forms.Label lblMarmellata;
+        private System.Windows.Forms.Label lblCottura;
+        private System.Windows.Forms.Label lblDiametro;
+        private System.Windows.Forms.Label lblDecorazione;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.ComboBox cmbFrolla;
+        private System.Windows.Forms.ComboBox cmbMarmellata;
+        private System.Windows.Forms.TextBox txtTempoCottura;
+        private System.Windows.Forms.TextBox txtDiametro;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.RadioButton rdbT;
+        private System.Windows.Forms.RadioButton rdbF;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRicerca;
+        private System.Windows.Forms.Button btnAggiungi;
     }
 }
 
--- C#/EsRicettaDigBM/EsRicettaDigBM/Form1.cs
+++ C#/EsRicettaDigBM/EsRicettaDigBM/Form1.cs
@@ -2,19 +2,219 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Deployment.Application;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
 
 namespace EsRicettaDigBM
 {
     public partial class Form1 : Form
     {
+        public enum Frolla
+        {
+            Classica,
+            Integrale,
+            Sablé,
+            Montana,
+            Milano,
+            al_Cacao
+        }
+
+        public enum Marmellata
+        {
+            Arance,
+            Limoni,
+            Mandarini,
+            Albicocche,
+            Fragole,
+            Pesche
+        }
+
+        public struct Ricetta
+        {
+            public int id;
+            public Frolla frolla;
+            public Marmellata marmellata;
+            public int tempo;
+            public int diametro;
+            public char decorazione;
+            public string name;
+
+
+            public Ricetta(int id, Frolla frolla, Marmellata marmellata, int tempo, int diametro, char decorazione, string name)
+            {
+                this.id = id;
+                this.frolla = frolla;
+                this.marmellata = marmellata;
+                this.tempo = tempo;
+                this.diametro = diametro;
+                this.decorazione = decorazione;
+                this.name = name;
+            }
+
+            public string stampa()
+            {
+                return $"N° {this.id} -name : {this.name} -frolla : {this.frolla} -marmellata : {this.marmellata} -tempo : {this.tempo} -diametro : {this.diametro} -decorazione{this.decorazione}";
+            }
+
+            public string print()
+            {
+                return $"{this.id};{this.name};{this.frolla};{this.marmellata};{this.tempo};{this.diametro};{this.decorazione}";
+            }
+        }
+
+        public string namefile = "ricette.csv";
+        public string[] riga, elementi;
+        public List<Ricetta> lstRicette;
+        public Ricetta ricetta;
+        public int count;
+        
         public Form1()
         {
             InitializeComponent();
+            cmbFrolla.DataSource = Enum.GetValues(typeof(Frolla));
+            cmbMarmellata.DataSource = Enum.GetValues(typeof(Marmellata));
+            lstRicette = new List<Ricetta>();
+            riga = new string[10];
+            elementi = new string[10];
+            count = 0;
+            download();
+        }
+
+        public void download()
+        {
+            if (File.Exists(namefile))
+            {
+                riga = File.ReadAllLines(namefile);
+                for(int i=1; i<riga.Length; i++)
+                {
+                    elementi = riga[i].Split(';');
+                    Estrai();
+                    count++;
+                }
+                CaricaLista();
+            }
+        }
+
+        public void Estrai()
+        {
+            ricetta.id = int.Parse(elementi[0]);
+            ricetta.name = elementi[1];
+            Frolla.TryParse(elementi[2], ignoreCase: true, out Frolla frolla);
+            ricetta.frolla = frolla;
+            Marmellata.TryParse(elementi[3], ignoreCase: true, out Marmellata marmellata);
+            ricetta.marmellata = marmellata;
+            ricetta.tempo = int.Parse(elementi[4]);
+            ricetta.diametro = int.Parse(elementi[5]);
+            ricetta.decorazione = char.Parse(elementi[6]);
+            lstRicette.Add(ricetta);
+        }
+
+        private void txtTempoCottura_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && e.KeyChar != '-')
+            {
+                e.KeyChar = (char)0;
+            }
+        }
+
+        private void txtDiametro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != '\b' && e.KeyChar != '-')
+            {
+                e.KeyChar = (char)0;
+            }
+        }
+
+        private void btnAggiungi_Click(object sender, EventArgs e)
+        {
+            if (Controllo())
+            {
+                ricetta.id = count;
+                ricetta.name = txtNome.Text;
+                ricetta.frolla = (Frolla)cmbFrolla.SelectedIndex;
+                ricetta.marmellata = (Marmellata)cmbMarmellata.SelectedIndex;
+                ricetta.tempo = int.Parse(txtTempoCottura.Text);
+                ricetta.diametro = int.Parse(txtDiametro.Text);
+                if (rdbF.Checked) ricetta.decorazione = 'F';
+                else ricetta.decorazione = 'T';
+                lstRicette.Add(ricetta);
+                CaricaLista();
+                count++;
+            }
+        }
+
+        public bool Controllo()
+        {
+            if(cmbFrolla.SelectedIndex == -1)
+            {
+                MessageBox.Show("Inserisci il tipo di Frolla");
+                return false;
+            }
+            if(cmbMarmellata.SelectedIndex == -1)
+            {
+                MessageBox.Show("Inserisci il tipo di marmellata");
+                return false;
+            }
+            if(int.Parse(txtTempoCottura.Text)<=0 || int.Parse(txtTempoCottura.Text) >= 1440)
+            {
+                MessageBox.Show("Inserisci un tempo di cottura valido : >0 e <1440 min (24 ore)");
+                return false;
+            }
+            if(int.Parse(txtDiametro.Text) <= 0 || int.Parse(txtDiametro.Text) >= 120)
+            {
+                MessageBox.Show("Inserisci un diametro valido : >0 e < 120 min (1,20 m)");
+                return false;
+            }
+            if(txtNome.Text == "")
+            {
+                MessageBox.Show("Inserisci un nome alla ricetta");
+                return false;
+            }
+            if(!rdbT.Checked && !rdbF.Checked)
+            {
+                MessageBox.Show("Inserisci l'opzione di decorazione (SI/NO)");
+                return false;
+            }
+            return true;
+        }
+
+        private void btnRicerca_Click(object sender, EventArgs e)
+        {
+            string nome = Interaction.InputBox("Inserisci ricetta", "RICERCA", "");
+            if(nome.Length != 0)
+            {
+                lstElenco.Items.Clear();
+                foreach(Ricetta r in lstRicette)
+                {
+                    if (r.name == nome) lstElenco.Items.Add(r.stampa());
+                }
+            }
+        }
+
+        private void btnSsave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(namefile, "");
+            File.AppendAllText(namefile, "ID;NAME;FROLLA;MARMELLATA;TEMPO;DIAMETRO;DECORAZIONE" + "\n");
+            foreach (Ricetta r in lstRicette) File.AppendAllText(namefile, r.print() + "\n");
+
+        }
+
+        public void CaricaLista()
+        {
+            lstElenco.Items.Clear();
+            lstRicette = lstRicette.OrderBy(r => r.name).ToList();
+            foreach (Ricetta r in lstRicette)
+            {
+                lstElenco.Items.Add(r.stampa());
+            }
         }
     }
 }
