f62f7f642f3f2dcbaf09a2088cf2d67c20e248b9
BertoldoMatteo <14529942@itisrossi.vi.it>
2026-04-22 15:55:06 +0200

    TRY-CATCH

---
+++ C#/EsCatch1BM/EsCatch1BM/Calcolatrice.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EsCatch1BM
+{
+    internal class Calcolatrice
+    {
+        private double a;
+        private double b;
+        private double div;
+
+        public Calcolatrice()
+        {
+            a = 0;
+            b = 0;
+        }
+
+        public Calcolatrice(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public double Divisione(string num, string den)
+        {
+            try
+            {
+                a = double.Parse(num);
+                b = double.Parse(den); 
+                div = a / b;
+                if(div < 0)
+                {
+                    throw new ArgumentException();
+                }
+                if(b == 0)
+                {
+                    throw new DivideByZeroException();
+                }
+                return div;
+            }
+            catch (DivideByZeroException)
+            {
+                return -1; 
+            }
+            catch (FormatException)
+            {
+                return -2;
+            }
+            catch(ArgumentException)
+            {
+                return -3;
+            }
+            catch
+            {
+                return -4;
+            }
+        }
+    }
+}
---
+++ C#/EsCatch1BM/EsCatch1BM/Program.cs
@@ -0,0 +1,31 @@
+﻿using EsCatch1BM;
+
+string a, b;
+double div;
+Console.WriteLine("Il programma eseguirà la divisione tra due numeri positivi");
+Console.WriteLine("");
+Console.WriteLine("Inserisci il primo numero (a)");
+a = Console.ReadLine();
+Console.WriteLine("Inserisci il secondo numero (b)");
+b = Console.ReadLine();
+Calcolatrice ris = new Calcolatrice();
+div = ris.Divisione(a, b);
+switch (div)
+{
+    case -1:
+        Console.WriteLine("Errore: divisione per zero.");
+        break;
+    case -2:
+        Console.WriteLine("Errore: input non numerico");
+        break;
+    case -3:
+        Console.WriteLine("Errore: numero negativo presente");
+        break;
+    case -4:
+        Console.WriteLine("Errore: input non valido");
+        break;
+    default:
+        Console.WriteLine("Divisione eseguita con successo.");
+        Console.WriteLine($"{a}/{b} = {div}");
+        break;
+}
